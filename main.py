33a9301302e1cadb1177cabf0e646eb5d552728e
Stefano <stefano.ziantoni20@gmail.com>
2020-09-10 22:33:50 +0200

    Initial Commit

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 import numpy
 import re
 
+stopwords = open("stopwords.txt").read().splitlines()
 whitelist = set('abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ ')
 db = pd.read_csv(r"C:\Users\Stefano\Desktop\films.csv", sep=';', header=None)
 
@@ -27,7 +28,7 @@ y=0
 for x in db[1]:
     plot = str(x)
     plot = ''.join(filter(whitelist.__contains__, plot))
-    bagsOfWords[y][1] = plot.split(' ')
+    bagsOfWords[y][1] = plot.lower().split(' ')
     y+=1
 
 uniqueWords = bagsOfWords[0][1]
@@ -37,14 +38,94 @@ for x in range(1, len(bagsOfWords)):
 
 #creo il dizionario con le occorrenze di ogni parola
 
-dictionary = pd.DataFrame(data=None,
-                          index=titles,
-                          columns=uniqueWords)
+dictionary = numpy.empty(shape=(4806,2), dtype=object)
 
+y=0
+for x in range(0, len(bagsOfWords)):
+    numOfWords = dict.fromkeys(uniqueWords, 0)
+    for word in bagsOfWords[y][1]:
+        numOfWords[word] += 1
+    dictionary[y][0] = titles[y]
+    dictionary[y][1] = numOfWords
+    y += 1
 
+#calcolo di tf-idf
 
+#TF
+def TFcomputation(words, bag):
+    TFcount= {}
+    bagCount = len(bag)
+    for word in words:
+        TFcount[word] = words[word] / len(bag)
+    return TFcount
 
+#IDF
+def IDFcomputation(documents):
+    import math
+    NDocs = len(documents)
+    idfDict = dict.fromkeys(documents[0].keys(), 0)
+    for document in documents:
+        for word, val in document.items():
+            if val > 0:
+                idfDict[word] += 1
 
+    for word, val in idfDict.items():
+        idfDict[word] = math.log(NDocs / float(val))
+    return idfDict
+
+TFIDF_Array = numpy.empty(shape=(4806,2), dtype=object)
+#TF-IDF
+def TFIDF_Matrix_Computation(TFMatrix, Idf):
+    y=0
+    for i in TFMatrix[:,1]:
+        tfidf = {}
+        for word, val in i.items():
+            if val > 0:
+                tfidf[word] = val * Idf[word]
+            else:
+                tfidf[word] = 0
+        TFIDF_Array[y][0]= titles[y]
+        TFIDF_Array[y][1]= sorted(tfidf.items(), key=lambda values: values[1], reverse=True)
+        y += 1
+
+TFMatrix = numpy.empty(shape=(4806,2), dtype=object)
+
+for x in range(0, len(bagsOfWords)):
+    tfCurrent = TFcomputation(dictionary[x][1], bagsOfWords[x][1])
+    TFMatrix[x][0] = titles[x]
+    TFMatrix[x][1] = tfCurrent
+
+Idf = IDFcomputation(dictionary[:,1])
+
+TFIDF_Matrix_Computation(TFMatrix, Idf)
+
+keywords = []
+
+for i in range(0, len(TFIDF_Array)):
+   words = TFIDF_Array[i][1]
+   for j in range(0, 9):
+       if len(words[j][0])>2:
+           if words[j][0] not in stopwords:
+               keywords.append(words[j][0])
+
+#Lista delle 25.000 parole chiave migliori usate per descrivere i 5000 film
+keywords = sorted(list(dict.fromkeys(keywords)))
+'''
+#KEYWORD TESTER
+s = 'The film that i want to see would have action scenes, a alien invasion or a war conflict, i prefer to see a ' \
+    'American film because it is my favourite genre, so i want action, guns, explosion, car race, and a lovestory if ' \
+    'it is possible'
+
+w = s.lower().split(' ')
+kw = []
+for word in w:
+    if word in keywords:
+        kw.append(word)
+
+pprint(kw)
+'''
+
+print("READY!")
 
 def on_chat_message(msg):
     content_type, chat_type, chat_id = telepot.glance(msg)  # glance lavora con ogni tipo di messaggio
@@ -55,14 +136,22 @@ def on_chat_message(msg):
     pprint(msg['text'])
     saluti = ["Ciao", "Salve", "Hi", "ciao", "salve", "hi"]
     if any(x in msg['text'] for x in saluti):
-        bot.sendMessage(chat_id, "Ciao caro! Scrivi l'username di un profilo Instagram e ti darò tutte le statistiche")
+        bot.sendMessage(chat_id, "Tell me")
     else:
+        w = msg['text'].lower().split(' ')
+        kw = []
+        kw_string = ''
+        for word in w:
+            if word in keywords:
+                kw.append(word)
+                kw_string = kw_string + str(word) + ' | '
         '''
         if content_type == 'text':
             bot.sendMessage(chat_id, "Non scrivere un messaggio usa la Inline Keyboard", reply_markup=keyboard)
         '''
         # ottenere le stats di youtube dato username
         try:
+            '''
             sauce = urllib.request.urlopen('https://www.instagram.com/' + msg['text'] + '/').read()
             soup = bs.BeautifulSoup(sauce, 'html.parser')
             # Cercare i tag span con classe 'abput-stats' (su yt sono chiamate cosi le info nell HTML)
@@ -77,7 +166,7 @@ def on_chat_message(msg):
                         answers = answer.split("Posts")
                         bot.sendMessage(chat_id, "Stats-> " + answers[0] + "Posts")
                         bot.sendMessage(chat_id, "Nome-> " + answers[1])
-            '''
+            
             #manda DOCUMENTO ALL'UTENTE CON I DATI PRELEVATI
             filename = "report"+msg['text']+".txt"
             with open(filename, "a") as out:
@@ -86,6 +175,7 @@ def on_chat_message(msg):
             bot.sendDocument(chat_id, open(filename, 'rb'))
             
             '''
+            bot.sendMessage(chat_id, 'KeyWords: ' + kw_string)
         except urllib.error.HTTPError as err:
             if err.code == 404:
                 bot.sendMessage(chat_id, "Il profilo IG non è stato trovato :(")
