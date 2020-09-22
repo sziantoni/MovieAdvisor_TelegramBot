d1382e1ff85341962574087931e45fc59c9338dd
Stefano <stefano.ziantoni20@gmail.com>
2020-09-22 22:10:16 +0200

    other improvements

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import time
+from pprint import pprint
 import pandas as pd
 import telepot
 from telepot.loop import MessageLoop
 import keywordConstructor as kc
 import sparql
 from SPARQLWrapper import SPARQLWrapper, JSON
+import spacy
 
 movies_genres = ['crime', 'action', 'adventure', 'comedy', 'drama', 'fantasy', 'historical', 'horror', 'mystery'
     , 'romance', 'saga', 'satire', 'thriller', 'science', 'urban', 'western', 'animation', 'cowboys', 'country',
@@ -13,11 +15,7 @@ s = sparql.Service(endpoint='http://dbpedia.org', qs_encoding="uft-8", method="G
 punctuation = set("!@#$-%^&*()_+<>?:.,;")
 db = pd.read_csv(r"C:\Users\Stefano\Desktop\films.csv", sep=';', header=None)
 keywords_first, keyword_second, keyword_general = kc.keywordGenerator(db)
-
-ciao = 1
-
-
-# keywords = ['action', 'police', 'american', 'army', 'chase', 'agencies', 'cia', 'fbi', 'special', 'effect']
+nlp = spacy.load("en_core_web_sm")
 
 
 def on_chat_message(msg):
@@ -28,6 +26,9 @@ def on_chat_message(msg):
                                  "\nWrite a short description about the type of film you want to see\nthe longer"
                                  " description provided, the better result you get\n\nGive me a description")
     else:
+        doc = nlp(msg['text'])
+        for chunk in doc.noun_chunks:
+            print('\n' + chunk.text)
         extra_part = ''
         kw_f = []
         kw_f_words = []
@@ -35,128 +36,177 @@ def on_chat_message(msg):
         kw_g = []
         kw = []
         kw_string = ''
-        w = msg['text']
+        w = ''
+        for token in doc:
+            if token.pos_ != 'VERB':
+                w = w + ' ' + token.text
         for p in punctuation:
             if p in w:
                 w = w.replace(p, "")
         w = w.lower().split(' ')
-
-        for i in range(0, len(keywords_first)-1):
+        genre_score = '0'
+        for k in w:
+            if k in movies_genres:
+                if genre == '':
+                    genre = '^(?=.*' + k + ')'
+                else:
+                    genre = genre + '(?=.*' + k + ')'
+        if genre != '':
+            genre = genre + '.*$'
+            genre_score = '10'
+        else:
+            genre = 'award'
+        pprint(keywords_first)
+        for i in range(0, len(keywords_first) - 1):
             for word in w:
-                if word == keywords_first[i][0] and "ing" not in word and word not in kw_f_words:
+                if (word == keywords_first[i][0] or (word in keywords_first[i][0] and len(keywords_first[i][0]) - len(
+                        word) < 1)) and "ing" not in word and word not in kw_f_words and len(word) > 2:
                     kw_f.append(keywords_first[i])
                     kw_f_words.append(word)
                     kw.append(word)
                     kw_string = kw_string + '1) ' + str(word) + ' | '
 
-        for word in w:
-            if word in keyword_second or "ing" in word:
-                if word not in kw_s:
-                    if word not in kw_f_words:
+        for i in range(0, len(keyword_second) - 1):
+            for word in w:
+                if word == keyword_second[i][0] or "ing" in word and len(word) > 2:
+                    if word not in kw_string:
                         kw_s.append(word)
                         kw.append(word)
                         kw_string = kw_string + '2) ' + str(word) + ' | '
-            elif word in keyword_general:
-                if word not in kw_g:
-                    if word not in kw_f_words:
-                        if word not in kw_s:
-                            kw_g.append(word)
-                            kw.append(word)
-                            kw_string = kw_string + '3) ' + str(word) + ' | '
-        if len(kw) > 0:
-            top_word = ''
-            for k in kw:
-                if k in movies_genres:
-                    genre = k
-            if genre == '':
-                genre = 'award'
+        for word in w:
+            if word in keyword_general:
+                if word not in kw_string and len(word) > 2:
+                    if word not in kw_s:
+                        kw_g.append(word)
+                        kw.append(word)
+                        kw_string = kw_string + '3) ' + str(word) + ' | '
 
+        kw_f.sort(key=lambda tup: tup[1], reverse=True)
+        if len(kw) > 0:
+            bot.sendMessage(chat_id, "Ok ok I'm searching......\n")
+            top_word = ''
             if len(kw_f) > 0:
                 if genre != kw_f[0][0]:
                     top_word = kw_f[0][0]
-                elif len(kw_f)>1:
+                elif len(kw_f) > 1:
                     top_word = kw_f[1][0]
-            kw_f = sorted(kw_f, key=lambda x: x[1], reverse=True)
-            query_first_part = 'PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> ' \
-                               'PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> ' \
-                               'PREFIX dct: <http://purl.org/dc/terms/> ' \
-                               'PREFIX dbo: <http://dbpedia.org/ontology/> ' \
-                               'PREFIX dbpprop: <http://dbpedia.org/property/> ' \
-                               'PREFIX dbc: <http://dbpedia.org/resource/Category:> ' \
-                               'SELECT DISTINCT ?id ?movie ?title ?score ?year1 ?abstract ?link ' \
-                               'WHERE{ ' \
-                               'SELECT DISTINCT ?id ?movie ?score ?abstract ?link ?year1 (str(?movie_title) as ?title) WHERE { ' \
-                               '?movie dbo:wikiPageID ?id. ' \
-                               ' ?movie rdf:type dbo:Film. ' \
-                               '?movie rdfs:label ?movie_title ' \
-                               'FILTER REGEX(?movie_title, "[Ff]ilm"). ' \
-                               '?movie foaf:isPrimaryTopicOf ?link  . ' \
-                               '?movie dbo:abstract ?abstract  FILTER langMatches(lang(?abstract), "EN") ' \
-                               'FILTER CONTAINS(?abstract, " ' + genre + ' "). ' \
-                                                                         '?movie dct:subject ?subject_.  ?subject_ rdfs:label ?subject1. '
+
+            nounArray = []
+            print('CHUNKER:\n')
+            for chunk in doc.noun_chunks:
+                for key in kw:
+                    if key in chunk.text:
+                        first = key
+                        for key_ in kw:
+                            if key_ in chunk.text and key_ != first:
+                                txt = nlp(chunk.text)
+                                result = ''
+                                for tk in txt:
+                                    pos = str(tk.pos_)
+                                    if pos != 'CONJ' and pos != 'PRON' and pos != 'ADV' and pos != 'ADP' and pos != 'PUNCT' and pos != 'SCONJ' and pos != 'DET':
+                                        result = result + ' ' + tk.text
+                                    testo = result
+                                    res = len(testo.split())
+                                    if testo not in nounArray and res > 1:
+                                        nounArray.append(testo)
+                                        print(testo + '\n')
+            if genre_score != '0':
+                query_first_part = 'PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> ' \
+                                   'PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> ' \
+                                   'PREFIX dct: <http://purl.org/dc/terms/> ' \
+                                   'PREFIX dbo: <http://dbpedia.org/ontology/> ' \
+                                   'PREFIX dbpprop: <http://dbpedia.org/property/> ' \
+                                   'PREFIX dbc: <http://dbpedia.org/resource/Category:> ' \
+                                   'SELECT DISTINCT ?id ?movie ?movie_title ?year1 ?abstract ?link ' \
+                                   ' WHERE { ' \
+                                   '?movie dbo:wikiPageID ?id. ' \
+                                   ' ?movie rdf:type dbo:Film. ' \
+                                   '?movie rdfs:label ?movie_title ' \
+                                   'FILTER REGEX(?movie_title, "[Ff]ilm"). ' \
+                                   '?movie foaf:isPrimaryTopicOf ?link  . ' \
+                                   '?movie dbo:abstract ?abstract  FILTER langMatches(lang(?abstract), "EN") ' \
+                                   'BIND((IF (REGEX(?abstract, "' + genre + '"), ' + genre_score + ' , 0)) AS ?genre_str). ' \
+                                                                                                   '?movie dct:subject ?subject_.  ?subject_ rdfs:label ?subject1. '
+            else:
+                query_first_part = 'PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> ' \
+                                   'PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> ' \
+                                   'PREFIX dct: <http://purl.org/dc/terms/> ' \
+                                   'PREFIX dbo: <http://dbpedia.org/ontology/> ' \
+                                   'PREFIX dbpprop: <http://dbpedia.org/property/> ' \
+                                   'PREFIX dbc: <http://dbpedia.org/resource/Category:> ' \
+                                   'SELECT DISTINCT ?id ?movie ?movie_title ?year1 ?abstract ?link ' \
+                                   ' WHERE { ' \
+                                   '?movie dbo:wikiPageID ?id. ' \
+                                   ' ?movie rdf:type dbo:Film. ' \
+                                   '?movie rdfs:label ?movie_title ' \
+                                   'FILTER REGEX(?movie_title, "[Ff]ilm"). ' \
+                                   '?movie foaf:isPrimaryTopicOf ?link  . ' \
+                                   '?movie dbo:abstract ?abstract  FILTER langMatches(lang(?abstract), "EN") ' \
+                                   'BIND((IF (REGEX(?abstract, "' + genre + '"), 3 , 0)) AS ?genre_str). ' \
+                                                                                                   '?movie dct:subject ?subject_.  ?subject_ rdfs:label ?subject1. '
             query_second_part = ''
             scorer = ''
             count = 0
             special = ''
+            for noun in nounArray:
+                nouner = ' BIND((IF (REGEX(?abstract, "' + str(noun) + '"), 5 , 0)) AS ?' + str(noun).replace(" ",
+                                                                                                              "_") + 's). '
+                scorer = scorer + '?' + str(noun).replace(" ", "_") + 's + '
+                query_second_part = query_second_part + nouner
+            scorer = scorer + ' ?genre_str +'
             for k in kw:
                 if k in kw_f_words:
-                    weight = '3'
+                    weight = '4'
                 elif k in kw_s:
                     weight = '2'
                 else:
                     weight = '1'
-                if k == 'film' or k == 'movie' and k != genre and k != kw_f[0][0]:
+                if k == 'film' or k == 'movie' and k != kw_f[0][0] and len(k) > 2:
                     binder = ' BIND((IF (REGEX(?abstract, " [' + k[0].upper() + k[0].lower() + ']' + k[
                                                                                                      1:] + 's "), ' + weight + ', 0)) AS ?' + k.replace(
                         "-", "") + 's). '
-                    #if k in kw_f_words:
-                    #    special = ' BIND((IF  (REGEX(?subject1, "[' + k[0].upper() + k[0].lower() + ']' + k[
-                    #                                                                                      1:] + '"),  4 , 0)) AS ?' + k + '_sub ). '
                     query_second_part = query_second_part + binder
                     if count == len(kw) - 1:
-                        #if k in kw_f:
-                        #    scorer = scorer + '?' + k.replace("-", "") + '_sub +'
                         scorer = scorer + '?' + k.replace("-", "") + 's'
                     else:
                         scorer = scorer + '?' + k.replace("-", "") + 's + '
-                        #if k in kw_f:
-                        #    scorer = scorer + '?' + k.replace("-", "") + '_sub + '
                     count += 1
                     binder = ''
                     special = ''
-                elif k != genre:
+                elif k != kw_f[0][0] and len(k) > 2:
                     binder = ' BIND((IF (REGEX(?abstract, " [' + k[0].upper() + k[0].lower() + ']' + k[
                                                                                                      1:] + ' "), ' + weight + ' , 0)) AS ?' + k.replace(
                         "-", "") + '). '
-                    #if k in kw_f:
-                    #    special = ' BIND((IF  (REGEX(?subject1, "[' + k[0].upper() + k[0].lower() + ']' + k[
-                    #                                                                                      1:] + '"),  4 , 0)) AS ?' + k + '_sub ). '
                     query_second_part = query_second_part + binder
                     if count == len(kw) - 1:
-                        #if k in kw_f:
-                        #    scorer = scorer + '?' + k.replace("-", "") + '_sub + '
                         scorer = scorer + '?' + k.replace("-", "")
                     else:
                         scorer = scorer + '?' + k.replace("-", "") + ' + '
-                        #if k in kw_f:
-                        #    scorer = scorer + '?' + k.replace("-", "") + '_sub + '
                     count += 1
                     binder = ''
                     special = ''
-                elif k == genre:
+                elif k == kw_f[0][0] or len(k) <= 2:
                     count += 1
-
             query_third_part = ' FILTER langMatches(lang(?movie_title), "EN") . ' \
                                '?movie dct:subject ?subject. ' \
                                '?subject rdfs:label ?year . ' \
-                               'filter regex(?year, "\\\\d{4}.films") .' \
+                               ' filter regex(?year, "\\\\d{4}.films") .' \
                                ' BIND(REPLACE(?year, "[^\\\\b0-9\\\\b]", "") AS ?movie_year2) BIND(SUBSTR(str(?movie_year2), 0,  4) AS ?year1) ' \
-                               '}}group by ?title ORDER BY desc(?score) desc(?year1) limit 5'
-            if top_word != '':
-                extra_part = ' BIND((IF  (REGEX(?subject1, "[' + top_word[0].upper() + top_word[0].lower() + ']' + top_word[1:] + '"),  5 , 0)) AS ?special2 ). '
-                final_query = query_first_part + query_second_part + extra_part + '  BIND(( ?special2 + ' + scorer + ') as ?score). ' + query_third_part
-            else:
+                               ' FILTER(xsd:integer(?year1) > 1990) ' \
+                               '}ORDER BY desc(?score) desc(?year1)  limit 5'
+            if top_word != '' and scorer != '':
+                extra_part = ' BIND((IF  (REGEX(?subject1, "[' + top_word[0].upper() + top_word[
+                    0].lower() + ']' + top_word[1:] + '"),  5 , 0)) AS ?special2 ). '
+                if len(kw) > 1:
+                    final_query = query_first_part + query_second_part + extra_part + '  BIND(( ?special2 + ' + scorer + ') as ?score). ' + query_third_part
+                else:
+                    final_query = query_first_part + query_second_part + extra_part + '  BIND(( ?special2 ) as ?score). ' + query_third_part
+
+            elif top_word == '' and scorer != '':
+                scorer = scorer.replace('+ )', ' )')
                 final_query = query_first_part + query_second_part + '  BIND(( ' + scorer + ') as ?score). ' + query_third_part
+            else:
+                final_query = query_first_part + query_second_part + query_third_part
             print(final_query)
             print('\nKEYWORDS: ' + kw_string)
             print('---------------')
@@ -168,9 +218,9 @@ def on_chat_message(msg):
             abstracts = []
             links = []
             for result in ret["results"]["bindings"]:
-                titles.append(result["title"]["value"])
+                titles.append(result["movie_title"]["value"])
                 abstracts.append(
-                    (result["abstract"]["value"][:300] + '..') if len(result["abstract"]["value"]) > 300 else
+                    (result["abstract"]["value"][:300] + '....') if len(result["abstract"]["value"]) > 300 else
                     result["abstract"]["value"])
                 links.append(result["link"]["value"])
             bot.sendMessage(chat_id, "I suggest you..\n\n")
--- venv/keywordConstructor.py
+++ venv/keywordConstructor.py
@@ -4,13 +4,16 @@ import urllib.error
 import urllib.request
 from pprint import pprint
 import pandas as pd
+import spacy
 
 stopwords = open("stopwords.txt").read().splitlines()
-whitelist = set('abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ ')
+whitelist = set('abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890 -')
 prohibited = ['film', 'movie']
+nlp = spacy.load("en_core_web_sm")
+db_len = 6000
 
 def keywordGenerator(db):
-    bagsOfWords = numpy.empty(shape=(5000, 2), dtype=object)
+    bagsOfWords = numpy.empty(shape=(db_len, 2), dtype=object)
     titles = []
     y = 0
     for x in db[0]:
@@ -19,19 +22,24 @@ def keywordGenerator(db):
         y += 1
     y = 0
     for x in db[1]:
-        plot = str(x)
+        text = str(x).lower()
+        plot = text
         plot = ''.join(filter(whitelist.__contains__, plot))
-        bagsOfWords[y][1] = plot.lower().split(' ')
+        plot = plot.split(' ')
+        for p in plot:
+            if len(p) < 3:
+                plot.remove(p)
+        bagsOfWords[y][1] = plot
         y += 1
-
+    print('Bag of word fatto!')
     uniqueWords = bagsOfWords[0][1]
 
     for x in range(1, len(bagsOfWords)):
         uniqueWords = set(uniqueWords).union(set(bagsOfWords[x][1]))
 
     # creo il dizionario con le occorrenze di ogni parola
-
-    dictionary = numpy.empty(shape=(5000, 2), dtype=object)
+    print('Unique word fatto!')
+    dictionary = numpy.empty(shape=(db_len, 2), dtype=object)
 
     y = 0
     for x in range(0, len(bagsOfWords)):
@@ -43,7 +51,7 @@ def keywordGenerator(db):
         y += 1
 
     # calcolo di tf-idf
-
+    print('Dizionario fatto!')
     # TF
     def TFcomputation(words, bag):
         TFcount = {}
@@ -66,7 +74,7 @@ def keywordGenerator(db):
             idfDict[word] = math.log(NDocs / float(val))
         return idfDict
 
-    TFIDF_Array = numpy.empty(shape=(5000, 2), dtype=object)
+    TFIDF_Array = numpy.empty(shape=(db_len, 2), dtype=object)
 
     # TF-IDF
     def TFIDF_Matrix_Computation(TFMatrix, Idf):
@@ -74,51 +82,64 @@ def keywordGenerator(db):
         for i in TFMatrix[:, 1]:
             tfidf = {}
             for word, val in i.items():
-                if val > 0:
+                if val > 0 and len(word)>2:
                     tfidf[word] = val * Idf[word]
-                else:
-                    tfidf[word] = 0
             TFIDF_Array[y][0] = titles[y]
             TFIDF_Array[y][1] = sorted(tfidf.items(), key=lambda values: values[1], reverse=True)
             y += 1
 
-    TFMatrix = numpy.empty(shape=(5000, 2), dtype=object)
+    TFMatrix = numpy.empty(shape=(db_len, 2), dtype=object)
 
     for x in range(0, len(bagsOfWords)):
         tfCurrent = TFcomputation(dictionary[x][1], bagsOfWords[x])
         TFMatrix[x][0] = titles[x]
         TFMatrix[x][1] = tfCurrent
-
+    print('TF fatto!')
     Idf = IDFcomputation(dictionary[:, 1])
-
+    print('IDF fatto!')
+    del dictionary
     TFIDF_Matrix_Computation(TFMatrix, Idf)
-
+    print('TFIDF fatto!')
+    del TFMatrix, Idf
     keywords_general = []
     keywords_first = []
     keywords_second = []
+    kw_file1 = open("keyword_first_take.txt", "w")
+    kw_file2 = open("keyword_second_take.txt", "w")
+    kw_file3 = open("keyword_general_take.txt", "w")
     for i in range(0, len(TFIDF_Array)):
         words = TFIDF_Array[i][1]
-        for j in range(0, 8):
+        limit = 0
+        if len(words) > 10:
+            limit = 10
+        elif len(words) > 4:
+            limit = len(words) - 2
+        else:
+            limit = len(words)
+        for j in range(0, limit):
             if len(words[j][0]) > 3:
                 if words[j][0] not in stopwords and words[j][0] not in prohibited:
-                    if ("ing" not in str(word[j][0])) and  words[j][1] > 4 and words[j] not in keywords_first and words[j][0] not in keywords_second and words[j][0] not in keywords_general :
+                    if ("ing" not in str(words[j][0])) and  words[j][1] > 4.4 and words[j] not in keywords_first and words[j] not in keywords_second and str(words[j][0]) not in keywords_general :
                             keywords_first.append(words[j])
-                    elif "ing" not in str(word[j][0]) and words[j][1] > 2.8 and words[j] not in keywords_first and words[j][0] not in keywords_second and words[j][0] not in keywords_general:
-                            keywords_second.append(words[j][0])
-                    elif words[j] not in keywords_first and words[j][0] not in keywords_second and words[j][0] not in keywords_general:
+                            kw_file1.write("\n" + str(words[j]))
+                    elif "ing" not in str(words[j][0]) and words[j][1] > 2.0 and words[j] not in keywords_first and words[j] not in keywords_second and str(words[j][0]) not in keywords_general:
+                            keywords_second.append(words[j])
+                            kw_file2.write("\n" + str(words[j]))
+                    elif words[j][1] > 1.0 and words[j] not in keywords_first and words[j] not in keywords_second and words[j][0] not in keywords_general:
                         keywords_general.append(words[j][0])
+                        kw_file3.write("\n" + str(words[j][0]))
                     else:
                         j -= 1
                 else:
                     j -= 1
             else:
                 j -= 1
+    kw_file2.close()
+    kw_file1.close()
+    kw_file3.close()
 
     # Lista delle 25.000 parole chiave migliori usate per descrivere i 5000 film
-    keywords_first = sorted(list(dict.fromkeys(keywords_first)))
-    keywords_second = sorted(list(dict.fromkeys(keywords_second)))
-    keywords_general = sorted(list(dict.fromkeys(keywords_general)))
-    keywords_first.append("love")
+    keywords_first.append(("love", 7.0))
     keywords_second.append("lovestory")
     keywords_general.append("love story")
     return keywords_first, keywords_second, keywords_general
